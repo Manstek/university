91bf1cc3acf636cbc4beca07c3be72f4a2f66896
Александр Морозов <morozov460336@yandex.ru>
2024-09-29 22:20:18 +0300

    create views.py

---
+++ university/logical/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+
+import os
+
+def load_rules(filename):
+    rules = []
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            line = line.strip()
+            if line:
+                part_1, part_2 = line.split(" то ")
+                condition_1 = part_1.replace("Если ", "").strip()
+                condition_2 = part_2.strip()
+                rule = (condition_1.split(' = ')[0], 
+                        condition_1.split(' = ')[1], 
+                        condition_2.split(' = ')[0], 
+                        condition_2.split(' = ')[1])
+                rules.append(rule)
+    return rules
+
+rules = load_rules(os.path.join(os.path.dirname(__file__), 'rules.txt'))
+condition = []
+value = []
+for i in range(0, len(rules), 2):
+    condition.append((rules[i][0], rules[i][0]))
+    value.append((rules[i][1], rules[i][1]))
+
+
+class LogicChainForm(forms.Form):
+    initial_condition = forms.ChoiceField(label='Объект', choices=condition)
+    initial_value = forms.ChoiceField(label='Значение' ,choices=value)
---
+++ university/logical/views.py
@@ -0,0 +1,53 @@
+import os
+from django.shortcuts import render
+from .forms import LogicChainForm
+
+def load_rules(filename):
+    rules = []
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            line = line.strip()
+            if line:
+                part_1, part_2 = line.split(" то ")
+                condition_1 = part_1.replace("Если ", "").strip()
+                condition_2 = part_2.strip()
+                rule = (condition_1.split(' = ')[0], 
+                        condition_1.split(' = ')[1], 
+                        condition_2.split(' = ')[0], 
+                        condition_2.split(' = ')[1])
+                rules.append(rule)
+    return rules
+
+def logical_chain(initial_condition, initial_value, rules):
+    current_states = {initial_condition: initial_value}
+
+    changes = True
+    while changes:
+        changes = False
+        for rule in rules:
+            condition_1, value_1, condition_2, value_2 = rule
+            if condition_1 in current_states and current_states[condition_1] == value_1:
+                if condition_2 not in current_states:
+                    current_states[condition_2] = value_2
+                    changes = True
+    if len(current_states) == 1:
+        return False
+    return current_states
+
+def home(request):
+    result = {}
+    error = False
+    if request.method == 'POST':
+        form = LogicChainForm(request.POST)
+        if form.is_valid():
+            initial_condition = form.cleaned_data['initial_condition']
+            initial_value = form.cleaned_data['initial_value']
+            rules = load_rules(os.path.join(os.path.dirname(__file__), 'rules.txt'))
+            result = logical_chain(initial_condition, initial_value, rules)
+            if not result:
+                error = 'Ошибка ввода данных.'
+
+    else:
+        form = LogicChainForm()
+
+    return render(request, 'index.html', {'form': form, 'result': result, 'error': error})
